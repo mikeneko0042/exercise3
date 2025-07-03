d9b39cee30c5698e5a2db11ad630a4e70d909ec5
mikeneko0042 <168636908+mikeneko0042@users.noreply.github.com>
2025-07-03 17:13:37 +0900

    Dice game: initial revision

---
+++ saikoro.py
@@ -0,0 +1,10 @@
+import random
+
+print("Rolling the dice...")
+saikoro1=random.randint(1,6)
+saikoro2=random.randint(1,6)
+sum=saikoro1+saikoro2
+
+print(f"Die 1: {saikoro1}")
+print(f"Die 2: {saikoro2}")
+print(f"Total value: {sum}")
